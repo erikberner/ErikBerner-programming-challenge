08a4670d2ac31393df9b5df24eb3d4cf086c9d6f
bernererik <bernererik@web.de>
2026-01-21 19:33:23 +0100

    cleanup App.java

--- src/main/java/de/bcxp/challenge/App.java
+++ src/main/java/de/bcxp/challenge/App.java
@@ -15,20 +15,17 @@ public final class App {
     public static void main(String... args) {
 
         // --- TASK 1: Weather ---
-
-        // 1. SETUP
         Repository<WeatherDay> weatherRepo = new RepositoryCSV<>(",", new WeatherMapper());
-        Repository<Country> countryRepo = new RepositoryCSV<>(";", new CountryMapper());
-
-        // 2. SERVICES
         AnalysisService weatherService = new WeatherService(weatherRepo);
-        AnalysisService countryService = new CountryService(countryRepo);
 
-        // 3. EXECUTION
         String weatherResult = weatherService.analyze("weather.csv");
         System.out.printf("Day with smallest temperature spread: %s%n", weatherResult);
 
         System.out.println("--------------------------------------------------");
+
+        // --- TASK 2: Countries ---
+        Repository<Country> countryRepo = new RepositoryCSV<>(";", new CountryMapper());
+        AnalysisService countryService = new CountryService(countryRepo);
 
         String countryResult = countryService.analyze("countries.csv");
         System.out.printf("Country with highest population density: %s%n", countryResult);
